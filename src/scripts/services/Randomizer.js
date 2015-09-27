72e142a0bd44c947192e3f1f3bb516741ed3f75b
DmitryKosmich <kosmich27@mail.ru>
2015-09-27 21:07:31 +0200

    Method for mixing has been added

--- src/scripts/services/Randomizer.js
+++ src/scripts/services/Randomizer.js
@@ -8,13 +8,26 @@
     function Randomizer() {}
 
     /**
-     * This method should mix the pack
-     * @param {FOOL.classes.Game} game
+     * This method should mix cards
+     * @param {Array} cards
      */
-    Randomizer.prototype.shufflePack = function (game) {
-        var pack = game.getPack();
-        //todo: this method should mix the pack
+    Randomizer.prototype.shuffleTalon = function (cards) {
+        var i, mixedCards = mix(cards),
+            length = mixedCards ? mixedCards.length : 0;
+        for (i = 0; i < length; i += 1) {
+            cards.push(mixedCards[i]);
+        }
     };
+
+    function mix(cards) {
+        var randomNum, length = cards ? cards.length : 0, result = [];
+        while (length) {
+            randomNum = Math.random() * length;
+            result.push(cards.splice(randomNum, 1)[0]);
+            length -= 1;
+        }
+        return result;
+    }
 
     FOOL.randomizer = new Randomizer();
 
